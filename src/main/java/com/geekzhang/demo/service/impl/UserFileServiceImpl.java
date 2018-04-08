4810945d766023a8bed64d1650e9931636f670af
zhangpengzhi <zhang_pz@suixingpay.com>
2018-04-08 16:13:04 +0800

    添加日志

--- src/main/java/com/geekzhang/demo/service/impl/UserFileServiceImpl.java
+++ src/main/java/com/geekzhang/demo/service/impl/UserFileServiceImpl.java
@@ -358,7 +358,7 @@ public class UserFileServiceImpl implements UserFileService {
         //非永久分享并且redis中无分享码
         if (!"2".equals(userFile.getShareValid())
                 && StringUtils.isEmpty(redisClient.getCacheValue("shareFile" + userFile.getId()))) {
-            log.info("文件分享|文件非永久分享并且redis中无分享码");
+            log.info("文件分享|文件非永久分享并且redis中无分享码" + userFile.getShareValid() + "2".equals(userFile.getShareValid()));
             map.put("code", ResponseCode.FILE_SHARE_TIMEOUT.getCode());
             map.put("msg", ResponseCode.FILE_SHARE_TIMEOUT.getDesc());
             return map;
